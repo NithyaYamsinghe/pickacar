1d1a8d4bce470e7b115841d3ef66f740be663de4
yasmikash <yasmikash@gmail.com>
2020-04-25 23:02:44 +0530

    fixing user profile

--- app/src/main/java/com/orioton/pickacar/client/ActivityUserProfile.java
+++ app/src/main/java/com/orioton/pickacar/client/ActivityUserProfile.java
@@ -1,19 +1,35 @@
 package com.orioton.pickacar.client;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.orioton.pickacar.R;
+import com.orioton.pickacar.admin.AddNewCarActivity;
+import com.orioton.pickacar.client.model.User;
+import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ActivityUserProfile extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,6 +37,18 @@ public class ActivityUserProfile extends AppCompatActivity implements View.OnCli
     private Uri uriProfilePhoto;
 
     ImageView imgPofilePhoto;
+    TextView profileName;
+    TextView userEmail;
+    TextView userPhone;
+    TextView userName;
+
+    ProgressDialog progressDialog;
+
+    DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
+    private String databasePath = "users";
+    private String currentUserId;
 
 
     @Override
@@ -28,9 +56,51 @@ public class ActivityUserProfile extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        progressDialog = new ProgressDialog(ActivityUserProfile.this);
+
+        // showing the progress dialog
+        progressDialog.setTitle("Getting Profile...");
+        progressDialog.show();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // getting the current userId
+        currentUserId = mAuth.getCurrentUser().getUid();
+
         imgPofilePhoto = findViewById(R.id.img_profile_photo);
+        profileName = findViewById(R.id.tv_profile_name);
+        userName = findViewById(R.id.tv_name);
+        userEmail = findViewById(R.id.tv_email);
+        userPhone = findViewById(R.id.tv_phone);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference(databasePath);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot usersSnapshot: dataSnapshot.getChildren()) {
+                    User user = usersSnapshot.getValue(User.class);
+
+                    // getting the current user info from the db snapshot
+                    if (user.getUserId().equals(currentUserId)) {
+                        // showing user profile
+                        showUserProfile(user);
+
+                    }
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         imgPofilePhoto.setOnClickListener(this);
+
 
     }
 
@@ -67,5 +137,18 @@ public class ActivityUserProfile extends AppCompatActivity implements View.OnCli
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select profile photo to upload"), CHOOSE_PHOTO);
+    }
+
+    private void showUserProfile(User user) {
+        profileName.setText(user.getUserName());
+        userName.setText(user.getUserName());
+        userPhone.setText(user.getUserPhone());
+        userEmail.setText(user.getUserEmail());
+
+        // loading the profile photo
+        Picasso.get().load(user.getUserProfilePhotoUrl()).into(imgPofilePhoto);
+
+        progressDialog.dismiss();
+
     }
 }
---
+++ app/src/main/java/com/orioton/pickacar/client/model/User.java
@@ -0,0 +1,72 @@
+package com.orioton.pickacar.client.model;
+
+public class User {
+
+    private String userId;
+    private String userName;
+    private String userEmail;
+    private String userPhone;
+    private String userProfilePhotoUrl;
+    private String joinedDate;
+
+    public User() {
+
+    }
+
+    public User(String userId, String userName, String userEmail, String userPhone, String userProfilePhotoUrl, String joinedDate) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPhone = userPhone;
+        this.userProfilePhotoUrl = userProfilePhotoUrl;
+        this.joinedDate = joinedDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserProfilePhotoUrl() {
+        return userProfilePhotoUrl;
+    }
+
+    public void setUserProfilePhotoUrl(String userProfilePhotoUrl) {
+        this.userProfilePhotoUrl = userProfilePhotoUrl;
+    }
+
+    public String getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(String joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+}
